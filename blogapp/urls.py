16ca5e27563b478efe54e15fefbd3ffaf593a961
sewald00 <sewald00@gmail.com>
2018-12-30 15:49:11 -0700

    added ability for users to comment on posts

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import redirect
@@ -41,6 +41,19 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blogapp/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blogapp/add_comment_to_post.html', {'form': form})
 
 def signup(request):
     if request.method == 'POST':
